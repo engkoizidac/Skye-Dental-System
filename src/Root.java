58be57bbb90cc8a4b8662180b2ef8a6d0331ac5b
LESTER JOHN PAUL CADIZ <cadizlester@gmail.com>
2025-05-14 10:53:28 +0300

    Update Root.java
    clean root.java on un necessary spaces and info

--- src/Root.java
+++ src/Root.java
@@ -1,27 +1,11 @@
 
 import app.global.ReportFactory;
 import app.view.user.Login;
-import java.time.LocalDateTime;
-import java.util.Date;
 import javax.swing.UIManager;
 import static javax.swing.UIManager.getInstalledLookAndFeels;
 import static javax.swing.UIManager.setLookAndFeel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author EngkoiZidac
- */
 public class Root {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
                try {
             setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
@@ -41,11 +25,6 @@ public class Root {
         
         ReportFactory Factory = new ReportFactory();
         Factory.LoadReportBoot();
-        
-        
-        
-       // LocalDateTime myObj = LocalDateTime.now();
-       // System.out.println(myObj);
     }
     
 }
